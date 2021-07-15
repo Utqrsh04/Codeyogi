c31f17a03700450533b4591c2bc8a6fd6b92bf8f
Utkarsh <Utkarshrkt2001@gmail.com>
2021-07-16 00:53:14 +0530

    Added useState for Login form input

--- src/Pages/Login.page.tsx
+++ src/Pages/Login.page.tsx
@@ -1,15 +1,21 @@
-import { FC, memo } from "react";
+import React, { FC, memo, useState } from "react";
 import { Link } from "react-router-dom";
 import { FaLock, FaUserAlt } from "react-icons/fa";
 
 interface Props {}
 
 const Login: FC<Props> = (props) => {
+  const [data, setData] = useState({ email: "", password: "" });
+
+  const handleChnage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setData({ ...data, [event.target.name]: event.target.value });
+  };
+
   return (
     <div className=" w-1/2 h-screen">
       <div className=" mx-auto text-center pt-10 ">
         <h2 className=" text-4xl ">
-          Log in to <h2 className=" inline text-blue-500 ">Codeyogi</h2>
+          Log in to <p className=" inline text-blue-500 ">Codeyogi</p>
         </h2>
         <h2 className=" pt-5 ">
           New Here?{" "}
@@ -19,12 +25,12 @@ const Login: FC<Props> = (props) => {
         </h2>
       </div>
       <div className=" w-1/2 mx-auto pt-32 ">
-        <form className="mt-8 space-y-6" action="#" method="POST">
+        <form className="mt-8 space-y-6">
           <input type="hidden" name="remember" defaultValue="true" />
           <div className="rounded-md shadow-sm -space-y-px">
             <div className=" flex flex-row ">
               <label htmlFor="email-address" className="sr-only">
-                Username
+                Email
               </label>
               <FaUserAlt className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" />
               <input
@@ -32,10 +38,12 @@ const Login: FC<Props> = (props) => {
                 name="email"
                 type="email"
                 autoComplete="email"
+                value={data.email}
+                onChange={handleChnage}
                 required
                 className="appearance-none rounded-none relative block w-full px-5 py-2  placeholder-gray-400
                  text-gray-900 font-bold rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                placeholder="Username"
+                placeholder="Email"
               />
             </div>
             <div className=" flex flex-row items-center">
@@ -52,6 +60,8 @@ const Login: FC<Props> = (props) => {
                 name="password"
                 type="password"
                 autoComplete="current-password"
+                value={data.password}
+                onChange={handleChnage}
                 required
                 className="appearance-none rounded-none relative block w-full px-5 py-2 placeholder-gray-400 text-gray-900 rounded-b-md font-bold focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Password"
@@ -62,7 +72,7 @@ const Login: FC<Props> = (props) => {
           <div className="flex items-center justify-between">
             <div className="text-sm">
               <Link
-                to="/signup"
+                to="/forgot-password"
                 className="font-medium text-indigo-600 hover:text-indigo-500"
               >
                 Forgot your password?
@@ -72,17 +82,21 @@ const Login: FC<Props> = (props) => {
 
           <div className="w-20  ">
             <button
-              type="submit"
-              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              onClick={() => {
+                console.log(data);
+              }}
+              className=" group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
               <span className="absolute left-0 inset-y-0 flex items-center pl-3"></span>
               Log in
             </button>
           </div>
         </form>
-        <h2 className=" text-blue-500 pt-20 font-semibold ">© 2021 All Rights Reserved.</h2>
+        <h2 className=" text-blue-500 pt-20 font-semibold ">
+          © 2021 All Rights Reserved.
+        </h2>
         <Link to="/dashboard">
-          <span className=" text-green-500">Go to Dashboard</span>
+          <span className=" font-semibold text-black">Go to Dashboard</span>
         </Link>
       </div>
     </div>
