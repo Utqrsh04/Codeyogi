eecc92e8be7a74dc97843cfcb7eba0856f448ff9
Utkarsh <Utkarshrkt2001@gmail.com>
2021-07-22 22:28:49 +0530

    Created ProgressBar StoryBook

--- src/components/ProgressBar/ProgressBar.stories.tsx
+++ src/components/ProgressBar/ProgressBar.stories.tsx
@@ -1,18 +1,21 @@
 import ProgressBar from "./ProgressBar";
 import "../../index.css";
 
-
 const func = {
-  title: "My Awesome ProgressBar",
+  title: "My Awesome Component",
   component: ProgressBar,
   argTypes: {
+    theme: {
+      control: { type: "select" },
+    },
   },
 };
 
 export const Progress = (args: any) => <ProgressBar {...args}></ProgressBar>;
 
 Progress.args = {
-  progress : 70,
+  progress: 70,
+  theme : "Blue"
 };
 
 export default func;
--- src/components/ProgressBar/ProgressBar.tsx
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -1,31 +1,34 @@
 import { FC, memo } from "react";
 
 interface Props {
-  progress : number
+  progress: number;
+  theme: "Blue" | "Red" | "Gray";
 }
 
-const ProgressBar: FC<Props> = ({progress}) => {
+const ProgressBar: FC<Props> = ({ progress, theme }) => {
+  
+  let themeClass = "";
+  if (theme === "Red") themeClass = "bg-red-";
+  else if (theme === "Blue") themeClass = "bg-blue-";
+  else if (theme === "Gray") themeClass = "bg-gray-";
 
-  const width = progress/100;
-  const classname = "w-1/"+width;
-  console.log(width , classname);
-  
-  
   return (
-    <>
+    <div className=" max-w-2xl mx-auto ">
       <div className="space-y-3">
         <h2 className="w-9 ml-auto mr-6 ">{progress}%</h2>
-        <div className="bg-gray-200 rounded-full">
-          <div className=" h-2  bg-blue-700 rounded-full " 
-         style={{width: `${progress}%`}} ></div>
+        <div className={"rounded-full " + themeClass+"200"}>
+          <div
+            className={" h-2  rounded-full  " + themeClass+"500"}
+            style={{ width: `${progress}%` }}
+          ></div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
 ProgressBar.defaultProps = {
-  progress : 50,
+  progress: 50,
 };
 
 export default memo(ProgressBar);
