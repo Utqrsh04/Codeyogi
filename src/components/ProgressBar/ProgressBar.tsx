1b10741fbed8fc8f3cd92ef76a296135c037fd24
Utkarsh <Utkarshrkt2001@gmail.com>
2021-07-22 23:50:28 +0530

    made changes to Progress bar theme

--- src/components/ProgressBar/ProgressBar.tsx
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -6,10 +6,9 @@ interface Props {
 }
 
 const ProgressBar: FC<Props> = ({ progress, theme }) => {
-  let themeClass = "bg-blue-";
-  if (theme === "Red") themeClass = "bg-red-";
-  else if (theme === "Blue") themeClass = "bg-blue-";
-  else if (theme === "Gray") themeClass = "bg-gray-";
+  let themeClass = "bg-blue-500";
+  if (theme === "Red") themeClass = "bg-red-500";
+  else if (theme === "Gray") themeClass = "bg-gray-500";
 
   if (progress > 100) progress = 100;
   else if (progress < 0) progress = 0;
@@ -18,9 +17,9 @@ const ProgressBar: FC<Props> = ({ progress, theme }) => {
     <>
       <div className="space-y-3">
         <h2 className="w-9 ml-auto mr-6 ">{progress}%</h2>
-        <div className={"rounded-full " + themeClass + "200"}>
+        <div className="rounded-full bg-gray-200">
           <div
-            className={" h-2  rounded-full  " + themeClass + "500"}
+            className={" h-2  rounded-full  " + themeClass}
             style={{ width: `${progress}%` }}
           ></div>
         </div>
@@ -31,7 +30,7 @@ const ProgressBar: FC<Props> = ({ progress, theme }) => {
 
 ProgressBar.defaultProps = {
   progress: 50,
-  theme : "Blue"
+  theme: "Blue",
 };
 
 export default memo(ProgressBar);
