7323d0fb48966a85c47c42a55aa0e8bed3da8355
Utkarsh <Utkarshrkt2001@gmail.com>
2021-08-04 22:03:41 +0530

    changed image url of list card

--- src/components/ListCard/List.tsx
+++ src/components/ListCard/List.tsx
@@ -1,42 +1,42 @@
 import { FC, memo } from "react";
 
 interface Props {
-  idx : number ;
-  Name? : string;
-  Description? : string;
+  idx: number;
+  Name?: string;
+  Description?: string;
 }
 
-const List: FC<Props> = ({idx, Name , Description}) => {
-
-  
-
+const List: FC<Props> = ({ idx, Name, Description }) => {
   let themeClass = "";
-  if(idx % 2  === 0 )
-    themeClass = " bg-gray-200 "
-  else
-    themeClass = "bg-white"
+  if (idx % 2 === 0) themeClass = " bg-gray-200 ";
+  else themeClass = "bg-white";
 
   // console.log(idx , Name , Description);
-    
+
   return (
-      <div className={" px-3 h-12 w-full border border-gray-200 flex items-center " + themeClass}>
-        <img className=" w-10 h-10 rounded-full "
-          src="https://www.lifeofpix.com/wp-content/uploads/2016/12/avatar.png"
-          alt=""
-        />
-        <div className=" px-2 text-left ">
-          <h1 className=" font-medium ">{Name}</h1>
-          <h2 className=" text-xs ">{Description}</h2>
-        </div>
+    <div
+      className={
+        " px-3 h-12 w-full border border-gray-200 flex items-center " +
+        themeClass
+      }
+    >
+      <img
+        className=" w-10 h-10 rounded-full "
+        src="https://images.unsplash.com/photo-1607990281513-2c110a25bd8c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=547&q=80"
+        alt=""
+      />
+      <div className=" px-2 text-left ">
+        <h1 className=" font-medium ">{Name}</h1>
+        <h2 className=" text-xs ">{Description}</h2>
       </div>
+    </div>
   );
 };
 
 List.defaultProps = {
-  idx : 1 ,
-  Name : "Utkarsh" ,
-  Description : "Software Developer" 
-
+  idx: 1,
+  Name: "Utkarsh",
+  Description: "Software Developer",
 };
 
 export default memo(List);
